8ce9f5bf06420507a91edf110cb2b526e2956c0a
taesu <taesu@crscube.co.kr>
2024-02-09 16:14:32 +0900

    Initial commit

---
+++ src/main/kotlin/io/taesu/redisspring/RedisSpringApplication.kt
@@ -0,0 +1,11 @@
+package io.taesu.redisspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedisSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<RedisSpringApplication>(*args)
+}
---
+++ src/test/kotlin/io/taesu/redisspring/RedisSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.taesu.redisspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
